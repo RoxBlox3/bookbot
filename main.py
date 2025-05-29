a710849cb48efa66fa2537060ba1b142209ead23
RoxBlox3 <RoxBlox33@proton.me>
2025-05-29 22:52:59 +0200

    Made the path an argument instead of hardcoded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import char_counter, word_counter, sort
+import sys
 
 
 def get_book_text(filepath):
@@ -8,17 +9,23 @@ def get_book_text(filepath):
 
 
 def main():
-    count = word_counter(get_book_text("./books/frankenstein.txt"))
-    char_list = char_counter(get_book_text("./books/frankenstein.txt"))
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {count} total words")
-    print("--------- Character Count -------")
-    dict_list = sort(char_list)
-    for entry in dict_list:
-        print(f"{entry['char']}: {entry['num']}")
-    print("============= END ===============")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
+        print(path)
+        count = word_counter(get_book_text(path))
+        char_list = char_counter(get_book_text(path))
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {count} total words")
+        print("--------- Character Count -------")
+        dict_list = sort(char_list)
+        for entry in dict_list:
+            print(f"{entry['char']}: {entry['num']}")
+        print("============= END ===============")
 
 
 main()
